3464a3ce6715bf5c3ec30ee5322aabf0afa1822c
RuidgerKe <ruidgerke@gmail.com>
2023-01-25 15:10:49 -0500

    build 2

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -33,6 +33,22 @@ public class @PlayerBehavior : IInputActionCollection, IDisposable
                     ""expectedControlType"": ""Button"",
                     ""processors"": """",
                     ""interactions"": """"
+                },
+                {
+                    ""name"": ""Look"",
+                    ""type"": ""Value"",
+                    ""id"": ""adcec98b-20c0-4ef4-803b-faed31964704"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Attack"",
+                    ""type"": ""Button"",
+                    ""id"": ""94652175-de86-4ff0-87f7-d7053cc1539c"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
                 }
             ],
             ""bindings"": [
@@ -101,6 +117,28 @@ public class @PlayerBehavior : IInputActionCollection, IDisposable
                     ""action"": ""Jump"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""40320535-113e-4ea4-80bd-8c17b7a4c7c4"",
+                    ""path"": ""<Pointer>/delta"",
+                    ""interactions"": """",
+                    ""processors"": ""InvertVector2(invertX=false),ScaleVector2(x=0.03,y=0.03)"",
+                    ""groups"": """",
+                    ""action"": ""Look"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""c36d4be3-ea42-4907-99c7-fc6e8bdda82e"",
+                    ""path"": ""<Mouse>/leftButton"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Attack"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
                 }
             ]
         }
@@ -111,6 +149,8 @@ public class @PlayerBehavior : IInputActionCollection, IDisposable
         m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
         m_Player_Move = m_Player.FindAction("Move", throwIfNotFound: true);
         m_Player_Jump = m_Player.FindAction("Jump", throwIfNotFound: true);
+        m_Player_Look = m_Player.FindAction("Look", throwIfNotFound: true);
+        m_Player_Attack = m_Player.FindAction("Attack", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -162,12 +202,16 @@ public class @PlayerBehavior : IInputActionCollection, IDisposable
     private IPlayerActions m_PlayerActionsCallbackInterface;
     private readonly InputAction m_Player_Move;
     private readonly InputAction m_Player_Jump;
+    private readonly InputAction m_Player_Look;
+    private readonly InputAction m_Player_Attack;
     public struct PlayerActions
     {
         private @PlayerBehavior m_Wrapper;
         public PlayerActions(@PlayerBehavior wrapper) { m_Wrapper = wrapper; }
         public InputAction @Move => m_Wrapper.m_Player_Move;
         public InputAction @Jump => m_Wrapper.m_Player_Jump;
+        public InputAction @Look => m_Wrapper.m_Player_Look;
+        public InputAction @Attack => m_Wrapper.m_Player_Attack;
         public InputActionMap Get() { return m_Wrapper.m_Player; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -183,6 +227,12 @@ public class @PlayerBehavior : IInputActionCollection, IDisposable
                 @Jump.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
                 @Jump.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
                 @Jump.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Look.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnLook;
+                @Look.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnLook;
+                @Look.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnLook;
+                @Attack.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnAttack;
+                @Attack.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnAttack;
+                @Attack.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnAttack;
             }
             m_Wrapper.m_PlayerActionsCallbackInterface = instance;
             if (instance != null)
@@ -193,6 +243,12 @@ public class @PlayerBehavior : IInputActionCollection, IDisposable
                 @Jump.started += instance.OnJump;
                 @Jump.performed += instance.OnJump;
                 @Jump.canceled += instance.OnJump;
+                @Look.started += instance.OnLook;
+                @Look.performed += instance.OnLook;
+                @Look.canceled += instance.OnLook;
+                @Attack.started += instance.OnAttack;
+                @Attack.performed += instance.OnAttack;
+                @Attack.canceled += instance.OnAttack;
             }
         }
     }
@@ -201,5 +257,7 @@ public class @PlayerBehavior : IInputActionCollection, IDisposable
     {
         void OnMove(InputAction.CallbackContext context);
         void OnJump(InputAction.CallbackContext context);
+        void OnLook(InputAction.CallbackContext context);
+        void OnAttack(InputAction.CallbackContext context);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,41 +13,66 @@ public class PlayerController : MonoBehaviour
 
     PlayerBehavior behavior;
 
-    Vector2 xy;
+    Vector2 move;
     Vector2 lookVector;
     Vector3 cameraRotation;
 
     private float distanceToGround;
-    private bool isGrounded;
+    private bool isGrounded = true;
     private bool isWalking;
+    private Vector2 rotate;
 
     private void Awake()
     {
         behavior = new PlayerBehavior();
-        
-        behavior.Player.Move.performed += cntxt => xy = cntxt.ReadValue<Vector2>();
-        behavior.Player.Move.canceled += cntxt => xy = Vector2.zero;
+
+        behavior.Player.Move.performed += cntxt => move = cntxt.ReadValue<Vector2>();
+        behavior.Player.Move.canceled += cntxt => move = Vector2.zero;
 
         behavior.Player.Jump.performed += cntxt => Jump();
+
+        behavior.Player.Look.performed += cntxt => rotate = cntxt.ReadValue<Vector2>();
+        behavior.Player.Look.canceled += cntxt => rotate = Vector2.zero;
+
+        behavior.Player.Attack.performed += cntxt => Attack();
+
+        cameraRotation = transform.eulerAngles; //easier than imputting all seperate axis's
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     private void OnEnable()
     {
-        
+        behavior.Player.Enable();
     }
     private void Update()
     {
-        
-    }
+        cameraRotation = new Vector3(cameraRotation.x + rotate.y, cameraRotation.y + rotate.x, 0);
 
+        transform.eulerAngles = new Vector3(transform.rotation.x, cameraRotation.y, transform.rotation.z);
+        transform.Translate(Vector3.forward * move.y * Time.deltaTime * moveSpeed, Space.Self);
+        transform.Translate(Vector3.right * move.x * Time.deltaTime * moveSpeed, Space.Self);
+
+
+    }
+    private void LateUpdate()
+    {
+        //cam.transform.eulerAngles = new Vector3(cameraRotation.x, cameraRotation.x, cameraRotation.z);
+        cam.transform.rotation = Quaternion.Euler(cameraRotation);
+    }
     private void OnDisable()
     {
-        
+        behavior.Player.Disable();
     }
 
-    void Jump()
+    void Attack()
     {
 
-    } 
+    }
+
+    private void Jump()
+    {
+        if (!isGrounded) return;
+        rb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);   
+    }
 
 }
